1249ebb42254ec9b3df7f0b96978df80a2c15c33
yoneda1013 <chat.414.kome@gmail.com>
2022-05-21 14:24:45 +0900

    拡張子の統一

--- src/compornents/Deadline.js
+++ src/compornents/Deadline.js
@@ -1,10 +1,8 @@
 import React from "react";
 import DatePicker from "react-datepicker";
-import styled from "styled-components";
 
 export const Deadline = (deadlineDate, onChange) => {
   return (
-    // <div className="deadline"></div>
     <Wrapper>
       <Label>入稿締切</Label>
       <DatePicker
